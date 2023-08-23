e1f4ab0e9290ced80af9ceb13ac50838c9cfc07d
Barbra <barbrakachembere@gmail.com>
2023-08-22 19:42:02 -0700

    printf main.h

---
+++ c-printf.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	int count=0;
+	va_list args;
+
+	if(format == NULL)
+
+	{
+		return (-1);
+	}
+
+	va_start (args, format);
+
+	while(*format) 
+	{
+
+		if (*format != '%') 
+	
+		{
+			write(1, format, 1);
+			count++;
+
+		}
+		else
+		{
+			format ++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			if (*format == '%')
+			{
+				write(1, format, 1);
+                        	count++;
+			}
+			else if (*format  == 'c')
+                        {
+                                char c = va_arg(args, int);
+                                write(1, &c, 1);
+				count++;
+                        }
+			else if (*format  == 's')
+                        {
+				char *str = va_arg(args, char*);
+	 			int len = 0;
+
+				while(str[len] != '\0')
+					
+					len++;
+				write(1, str, len);
+                                count += len;
+                        }
+		}
+		format++;
+	}
+	va_end(args);
+	return count;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+    
+	len = _printf("Let's try to printf a simple sentence.\n");
+	return 0;
+}
+	
