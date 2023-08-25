a38ebfb91f0dc1b08d57b3b9caf54e4e43fa6a35
iamtapiwa <tapsdoc23@gmail.com>
2023-08-25 06:14:01 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -56,16 +56,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return count;
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	_printf("Let's try to printf a simple sentence.\n");
-	return 0;
-}
-	
+}	
